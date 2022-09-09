a699bc3977881c799532aa89f727930469b3158f
natibi121 <natibi279@gmail.com>
2022-09-09 07:42:45 +0200

    kesz

---
+++ számítás.js
@@ -0,0 +1,9 @@
+/*Terület = a * b
+Kerület = 2*(a+b)*/
+
+function szamol(){
+    a = parseInt(document.getElementById('a').value);
+    b = parseInt(document.getElementById('b').value);
+    parseInt(document.getElementById('k').value = 2*(a+b) )
+    parseInt(document.getElementById('t').value = a*b )
+}
