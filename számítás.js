07227f26e94d892d9ebcbdbc27b72fcdc1f4da69
natibi121 <natibi279@gmail.com>
2022-09-09 12:29:31 +0200

    folytatas3

--- számítás.js
+++ számítás.js
@@ -8,9 +8,9 @@ function szamol(){
     parseInt(document.getElementById('t_tegla').value = a*b )
 }
 function k_szamol(mibol){
-    if (mibol = "r"){
+    if (mibol == "r"){
     let r = parseInt(document.getElementById('r').value);
-    document.getElementById('k_kor').value = Math.round(2* r * Math.PI); 
+    document.getElementById('k_kor').value = Math.round(2 * r * Math.PI); 
     document.getElementById('t_kor').value = r*r*Math.PI;
     }
     else if (mibol == "k"){
@@ -26,12 +26,27 @@ function k_szamol(mibol){
         document.getElementById('k_kor').value = 2 * r * Math.PI
     }
 }
+function haromszog_szamol(){
+    let a = parseInt(document.getElementById('h_a').value);
+    let b = parseInt(document.getElementById('h_b').value);
+    let c = parseInt(document.getElementById('h_c').value);
+    let k = a + b + c;
+    let s = k / 2;
+    let t = Math.round(MATH.sqrt((s* (s -a) * (s -b) * (s -c))));
+    document.getElementById('k_haromszog').value = a + b + c;
+    document.getElementById('t_haromszog').value = a*b 
+}
+
 function valt(mire){
     document.getElementById('teglalap').style.display = "none";
     document.getElementById('kor').style.display = "none";
     document.getElementById('haromszog').style.display = "none";
-    document.getElementById('negyzet').style.display = "none";
-    document.getElementById('hatszog').style.display = "none";
+    // document.getElementById('negyzet').style.display = "none";
+    // document.getElementById('hatszog').style.display = "none";
+    let formak = document.getElementsByTagName('form');
+    for (i=0; i < formak.length;i++){
+        formak[i].style.display = "none";
+    }
     switch (mire){
         case 'kor':
             document.getElementById('kor').style.display = "block";
